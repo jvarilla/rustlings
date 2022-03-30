fe97ee9cd779ec8c33ab008ce688bc146de06376
Joseph Varilla <jvarilla981@gmail.com>
2022-03-29 22:49:10 -0500

    Completes variables exercise 5

--- exercises/variables/variables5.rs
+++ exercises/variables/variables5.rs
@@ -1,11 +1,10 @@
 // variables5.rs
 // Make me compile! Execute the command `rustlings hint variables5` if you want a hint :)
 
-// I AM NOT DONE
-
 fn main() {
     let number = "T-H-R-E-E"; // don't change this line
     println!("Spell a Number : {}", number);
-    number = 3;
+    // Shadow the variable to have a new one with the same name but a different type
+    let number = 3;
     println!("Number plus two is : {}", number + 2);
 }
